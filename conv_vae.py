0b65c8680f91af0b0ab051cf0666377eee5f4076
Xingzhi Sun <xingzhi.sun@yale.edu>
2022-05-04 17:44:42 -0400

    change face to coin

---
+++ conv_vae.py
@@ -0,0 +1,105 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+"""
+A Convolutional Variational Autoencoder
+"""
+class ConVAE(nn.Module):
+    def __init__(self): # assume img is square.
+        super(ConVAE, self).__init__()
+        self.n1 = 64
+        self.n2 = 64
+        self.n3 = 32
+        self.k = 5
+        self.n_fc = 64
+        self.n_channels = 3
+        # self.n_channels = 1
+        # self.fc_size = 128
+        self.fc_size = 8192
+        # Initializing the 2 convolutional layers and 2 full-connected layers for the encoder
+        self.encConv1 = nn.Conv2d(self.n_channels, self.n1, self.k, padding=(2, 2))
+        self.encConv2 = nn.Conv2d(self.n1, self.n2, self.k, padding=(2,2))
+        self.encConv3 = nn.Conv2d(self.n2, self.n3, self.k, padding=(2,2))
+        self.encFC1 = nn.Linear(self.fc_size, self.n_fc)
+        self.encFC2 = nn.Linear(self.fc_size, self.n_fc)
+
+        # Initializing the fully-connected layer and 2 convolutional layers for decoder
+        self.decFC1 = nn.Linear(self.n_fc, self.fc_size)
+        self.decConv3 = nn.ConvTranspose2d(self.n3, self.n2, self.k, padding=(2,2))
+        self.decConv2 = nn.ConvTranspose2d(self.n2, self.n1, self.k, padding=(2,2))
+        self.decConv1 = nn.ConvTranspose2d(self.n1, self.n_channels, self.k, padding=(2,2))
+
+        self.pool = nn.MaxPool2d(2)
+        self.upsampling = nn.Upsample(scale_factor=2)
+
+    def encoder(self, x):
+
+        # Input is fed into 2 convolutional layers sequentially
+        # The output feature map are fed into 2 fully-connected layers to predict mean (mu) and variance (logVar)
+        # Mu and logVar are used for generating middle representation z and KL divergence loss
+        # print(x.size())
+        x = F.leaky_relu(self.encConv1(x))
+        x = self.pool(x)
+        # print(x.size())
+        x = F.leaky_relu(self.encConv2(x))
+        x = self.pool(x)
+        # print(x.size())
+        x = F.leaky_relu(self.encConv3(x))
+        x = self.pool(x)
+        # print(x.size())
+        x = x.view(x.size()[0], -1)
+        mu = self.encFC1(x)
+        logVar = self.encFC2(x)
+        return mu, logVar
+
+    def reparameterize(self, mu, logVar):
+
+        #Reparameterization takes in the input mu and logVar and sample the mu + std * eps
+        std = torch.exp(logVar/2)
+        eps = torch.randn_like(std)
+        return mu + std * eps
+
+    def decoder(self, z):
+
+        # z is fed back into a fully-connected layers and then into two transpose convolutional layers
+        # The generated output is the same size of the original input
+        x = F.relu(self.decFC1(z))
+        # print(x.size())
+        x = x.view(-1, self.n3, 16, 16)
+        # print(x.size())
+        x = self.upsampling(x)
+        # print(x.size())
+        x = F.leaky_relu(self.decConv3(x))
+        x = self.upsampling(x)
+        # print(x.size())
+        x = torch.sigmoid(self.decConv2(x))
+        x = self.upsampling(x)
+        # print(x.size())
+        x = torch.sigmoid(self.decConv1(x))
+        return x
+
+    def forward(self, x):
+        # The entire pipeline of the VAE: encoder -> reparameterization -> decoder
+        # output, mu, and logVar are returned for loss computation
+        mu, logVar = self.encoder(x)
+        z = self.reparameterize(mu, logVar)
+        out = self.decoder(z)
+        return out, mu, logVar
+
+
+# def VAE_loss_function(recon_x, x, mu, logvar):
+#     # TO DO: Implement reconstruction + KL divergence losses summed over all elements and batch
+#     mseloss = nn.MSELoss()
+#     recon_loss = 30 * mseloss(recon_x, x)
+#     KLD = - 0.5 * (1 + logvar - torch.square(mu) - torch.exp(logvar)).mean()
+#     # see lecture 12 slides for more information on the VAE loss function
+#     # for additional information on computing KL divergence
+#     # see Appendix B from VAE paper:
+#     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+#     # https://arxiv.org/abs/1312.6114
+
+#     return recon_loss + KLD
---
+++ main.py
@@ -0,0 +1,113 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+from conv_vae import ConVAE#, VAE_loss_function
+import torchvision.transforms.functional as TF
+
+import os
+# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = 'cpu'
+print(device)
+
+"""
+Initialize Hyperparameters
+"""
+batch_size = 16
+learning_rate = 1e-4
+num_epochs = 10
+alpha = 0.1 # noise level
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
+"""
+Dataloader
+"""
+
+class ImgPrep:
+    def __init__(self):
+        pass
+    def __call__(self, x):
+        # x = TF.to_grayscale(x)
+        # x = TF.adjust_sharpness(x, 10)
+        x = TF.adjust_contrast(x, 2)
+        x = TF.autocontrast(x)
+        # x = TF.gaussian_blur(x, 3)
+        x = TF.equalize(x)
+        # x = TF.posterize(x, 1)
+        return x
+
+# data_path = 'archive/coins/data/train'
+# data_path = 'RRC-60/Observe/'
+# data_path = 'lfw-deepfunneled'
+data_path = 'data/'
+transform = transforms.Compose([transforms.Resize(128),
+                                transforms.CenterCrop(128),
+                                transforms.ColorJitter(0.1, 0.1, 0.1, 0.1),
+                                ImgPrep(),
+                                transforms.ToTensor(),
+                                ])
+dataset = datasets.ImageFolder(data_path, transform=transform)
+train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+
+
+# """
+# Create dataloaders to feed data into the neural network
+# Default MNIST dataset is used and standard train/test split is performed
+# """
+# # train_loader = torch.utils.data.DataLoader(
+# #     datasets.MNIST('data', train=True, download=True,
+# #                     transform=transforms.ToTensor()),
+# #     batch_size=batch_size, shuffle=True)
+# # test_loader = torch.utils.data.DataLoader(
+# #     datasets.MNIST('data', train=False, transform=transforms.ToTensor()),
+# #     batch_size=1)
+
+
+"""
+Initialize the network and the Adam optimizer
+"""
+model = ConVAE().to(device)
+model.apply(init_weights)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+"""
+Training the network for a given number of epochs
+The loss after every epoch is printed
+"""
+model.train()
+for epoch in range(num_epochs):
+    # print(len(train_loader))
+    for idx, data in enumerate(train_loader):
+        # print(idx)
+        imgs, labels = data
+        # add noise
+        imgs = imgs + alpha * torch.randn(imgs.size())
+        # print(labels.size())
+        imgs = imgs.to(device)
+
+        # Feeding a batch of images into the network to obtain the output image, mu, and logVar
+        out, mu, logVar = model(imgs)
+
+        # The loss is the BCE loss combined with the KL divergence to ensure the distribution is learnt
+        kl_divergence = 0.5 * torch.mean(-1 - logVar + mu.pow(2) + logVar.exp())
+        # loss = 20 * F.binary_cross_entropy(out, imgs) + kl_divergence
+        loss = 10 * F.mse_loss(out, imgs) + kl_divergence
+        # loss = VAE_loss_function(out, imgs, mu, logVar)
+        # Backpropagation based on the loss
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print('Epoch {}: Loss {}'.format(epoch, loss))
